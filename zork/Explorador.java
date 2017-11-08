4b90e7f3c9c8402448b6ad4e4e701025b1509dc6
TheArturthur <arturo.vidal.96@gmail.com>
2017-11-08 18:22:58 +0100

    Laboratorio 4

---
+++ zork/Explorador.java
@@ -0,0 +1,12 @@
+package aed.zork;
+
+import es.upm.aedlib.positionlist.*;
+
+
+
+public class Explorador {
+  
+  public static Pair<Object,PositionList<Lugar>> explora(Lugar inicialLugar) {
+    return null;
+  }
+}
